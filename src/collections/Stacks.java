aaef1fee726d8f0ec41e9f7dee6f059f72440f39
Manoj <manoj.delhi24@gmail.com>
2020-08-29 09:49:47 +0200

    simple example for Java Stack

---
+++ src/collections/Stacks.java
@@ -0,0 +1,19 @@
+package collections;
+import java.util.*;
+
+public class Stacks {
+    public static void main(String[] args) {
+
+        Stack<Integer> s = new Stack();
+
+//        System.out.println("Empty peek: " + s.peek()); // java.util.EmptyStackException
+        s.push(10);
+        s.push(20);
+//        s.add(50); // from collections API
+        System.out.println("peek: " + s.peek());
+        s.pop();
+        System.out.println("pop: " + s.pop());
+//        System.out.println("Empty pop: " + s.pop()); // EmptyStackException
+
+    }
+}
